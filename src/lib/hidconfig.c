fef12523d2ef78f7814c0b8a09233f4c168baeed
Handle <mail@bysb.net>
2025-01-29 19:20:45 +0800

    规范HID自定义通信规则，提供新配置项

--- src/lib/hidconfig.c
+++ src/lib/hidconfig.c
@@ -1,4 +1,7 @@
 #include "hidconfig.h"
+#include "roller.h"
+
+#include "string.h"
 
 uint8_t HIDCFG_Buffer_OUT[64] = {0x00};
 uint8_t HIDCFG_Buffer_IN[64]  = {0x00};
@@ -6,23 +9,74 @@ uint8_t HIDCFG_Buffer_IN[64]  = {0x00};
 static HidconfigData *dataReceive = (HidconfigData *)HIDCFG_Buffer_OUT;
 static HidconfigData *dataUpload  = (HidconfigData *)HIDCFG_Buffer_IN;
 
+#define HID_CFG_EP ENDP4
+
+void HIDCONFIG_Upload();
+void HIDCONFIG_Receive_Handler();
+
+/**
+ * Request
+ * 1 byte: Report ID
+ * 1 byte: Symbol
+ * 1 byte: Command
+ * 1 byte: Unused
+ * 60 bytes: Payload
+ *
+ * Response
+ * 1 byte: Report ID
+ * 1 byte: Symbol, same as request
+ * 1 byte: Command, same as request
+ * 1 byte: State
+ * 60 bytes: Payload
+ */
+
 void HIDCONFIG_Receive_Handler()
 {
   switch (dataReceive->reportID) {
     // Custom HIDConfigs
-    case 0xAA:
+    case 0xAA: {
+      memset(HIDCFG_Buffer_IN, 0, 64); // Clear buffer
+
+      dataUpload->reportID = 0xAA;
+      dataUpload->symbol   = dataReceive->symbol;
+
       switch (dataReceive->command) {
+        case GET_ROLLER_DATA: {
+          uint16_t rollerValue    = Roller_GetValue();
+          uint16_t rollerRawValue = Roller_GetRawValue();
+          // Upload roller data
+          dataUpload->command    = GET_ROLLER_DATA;
+          dataUpload->state      = STATE_OK;
+          dataUpload->payload[0] = (uint8_t)(rollerValue >> 8);
+          dataUpload->payload[1] = (uint8_t)(rollerValue & 0xFF);
+          dataUpload->payload[2] = (uint8_t)(rollerRawValue >> 8);
+          dataUpload->payload[3] = (uint8_t)(rollerRawValue & 0xFF);
+          break;
+        }
         case SET_ROLLER_OFFSET: {
           // Calc roller offset to 0x8000
           Roller_ResetOffset();
+
+          dataUpload->command = SET_ROLLER_OFFSET;
+          dataUpload->state   = STATE_OK;
           break;
         }
         default: {
+          dataUpload->command = CMD_NOT_SUPPORT;
+          dataUpload->state   = STATE_ERROR;
           break;
         }
       }
+      HIDCONFIG_Upload();
+      break;
+    }
     default: {
       break;
     }
   }
+}
+
+void HIDCONFIG_Upload()
+{
+  USBD_ENDPx_DataUp(HID_CFG_EP, HIDCFG_Buffer_IN, 64);
 }
--- src/lib/hidconfig.h
+++ src/lib/hidconfig.h
@@ -4,24 +4,26 @@
 #include "usb_lib.h"
 #include "usb_prop.h"
 
-#include "keyscan.h"
-#include "roller.h"
-
 typedef enum {
   SET_ROLLER_OFFSET = 0xA0,
+  GET_ROLLER_DATA   = 0xA1,
   UPDATE_FIRMWARE   = 0xF1,
+  CMD_NOT_SUPPORT   = 0xFF,
 } __packed HidconfigCommand;
-
 typedef struct {
   uint8_t reportID;
+  uint8_t symbol;
   HidconfigCommand command;
-  uint8_t payload[62];
+  uint8_t state;
+  uint8_t payload[60];
 } __packed HidconfigData;
+
+#define STATE_OK    0x01
+#define STATE_ERROR 0xFF
 
 void HIDCONFIG_Receive_Handler();
 
 extern uint8_t HIDCFG_Buffer_OUT[64];
 extern uint8_t HIDCFG_Buffer_IN[64];
-
 
 #endif
